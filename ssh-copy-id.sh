daabb5372e242bf0df0aa0889751290b84387a2a
sukileung423 <sukileung423@gmail.com>
2022-07-27 15:06:58 +0800

    support diff os

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -3,14 +3,33 @@
 HOST=$1
 
 if [[ $# -le 0  ]];then
-	echo 'Usage: ./ssh-copy-id.sh HOST@IP'
+	echo 'Usage: ./ssh-copy-id.sh HOST@IP '
 	exit 1
 fi
+
+echo "Your case number:"
+echo "1: To opnwrt"
+echo "2: To linux"
+#echo "1: linux to opnwrt"
+
+
+read OPTION
 
 echo 'Copying public key to destination node...'
 
 
-ssh ${HOST} "tee -a ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+case $OPTION in
+	1) 
+		ssh ${HOST} "tee -a /etc/dropbear/authorized_keys" < ~/.ssh/id_rsa.pub
+	;;
+	2)
+		ssh ${HOST} "tee -a ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+	;;
+esac
+
+
+
+#ssh ${HOST} "tee -a ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
 
 echo ''
 
